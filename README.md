3e0d36690d8a94c6bfdb5946c5561a60ead6d7b9
singhrahul03 <rahul86197@outlook.com>
2025-08-31 16:16:15 +0530

    new Commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# chit_chat_app
-
---
+++ backend/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/database.js";
+import userRoute from "./routes/userRoute.js";
+import messageRoute from "./routes/messageRoute.js";
+import cookieParser from "cookie-parser";
+import cors from "cors"
+
+dotenv.config({});
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+const corsOption = {
+    origin: 'http://localhost:5173', // change according to vite server
+    credential: true
+}
+app.use(cors(corsOption));
+
+// routes 
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/message", messageRoute);
+
+app.listen(PORT, () => {
+    connectDB();
+    console.log(`server listen at port ${PORT}`);
+})
